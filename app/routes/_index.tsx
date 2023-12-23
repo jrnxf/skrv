3cfc23af9970398069df956e81adb6a0d47a797a
Colby Thomas <colby@jrnxf.co>
2023-12-23 14:55:33 -0700

    added sonner

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
+import { toast } from "sonner";
 import { ModeToggle } from "~/components/mode-toggle";
 import { Button } from "~/components/ui/button";
+import { Toaster } from "~/components/ui/sonner";
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,8 +15,9 @@ export default function Index() {
   return (
     <div>
       <h1 className="text-3xl">skrv</h1>
-      <Button>click me!</Button>
+      <Button onClick={() => toast(Date.now())}>click me!</Button>
       <ModeToggle />
+      <Toaster />
     </div>
   );
 }
