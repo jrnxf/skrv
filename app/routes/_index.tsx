36e3e51fbbc78f0f37fc3c89ff8edbb4f85ee430
Colby Thomas <colby@jrnxf.co>
2023-12-23 14:38:02 -0700

    add shadcn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,6 +12,7 @@ export default function Index() {
   return (
     <div>
       <h1 className="text-3xl">skrv</h1>
+      <Button>click me!</Button>
     </div>
   );
 }
